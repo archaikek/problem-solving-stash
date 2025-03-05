ef299c965f5ab926c8c06f806ad300d2c93a8042
Filip <archaikek>
2025-03-05 23:45:34 +0100

    Moved everything to main because SPOJ only allows one file

--- Core/main.cpp
+++ Core/main.cpp
@@ -1,5 +1,145 @@
-#include "common_defines.h"
-#include "fast_io.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+
+#define UNSAFE_IO // comment this out if your program needs thread-safe IO
+#ifdef __unix__
+#include<unistd.h>
+#ifdef UNSAFE_IO
+#define _getchar getchar_unlocked
+#define _putchar putchar_unlocked
+#else
+#define _getchar getchar
+#define _putchar putchar
+#endif
+#else
+#define _getchar getchar
+#define _putchar putchar
+#endif
+
+#define ff first
+#define ss second
+#define eb emplace_back
+#define debug if(1)
+
+typedef long long int LL;
+typedef long double LD;
+typedef short int SH; 
+
+inline void readI(int *i)
+{
+	register int r = 0;
+	register char c = _getchar();
+	int z = (c == '-' ? -1 : 1);
+
+	while (c < '0' || c > '9')
+	{
+		c = _getchar();
+		if (c == '-') z = -1;
+	}
+	while (c >= '0' && c <= '9')
+	{
+		r = 10 * r + c - '0';
+		c = _getchar();
+	}
+	*i = r * z;
+}
+inline void readUI(int *i)
+{
+	register int r = 0;
+	register char c = _getchar();
+
+	while (c < '0' || c > '9')
+	{
+		c = _getchar();
+	}
+	while (c >= '0' && c <= '9')
+	{
+		r = 10 * r + c - '0';
+		c = _getchar();
+	}
+	*i = r;
+}
+inline void readLL(long long int *l)
+{
+	register long long int r = 0;
+	register char c = _getchar();
+	int z = (c == '-' ? -1 : 1);
+
+	while (c < '0' || c > '9')
+	{
+		c = _getchar();
+		if (c == '-') z = -1;
+	}
+	while (c >= '0' && c <= '9')
+	{
+		r = 10 * r + c - '0';
+		c = _getchar();
+	}
+	*l = r * z;
+}
+inline void readULL(long long int *l)
+{
+	register long long int r = 0;
+	register char c = _getchar();
+
+	while (c < '0' || c > '9')
+	{
+		c = _getchar();
+	}
+	while (c >= '0' && c <= '9')
+	{
+		r = 10 * r + c - '0';
+		c = _getchar();
+	}
+	*l = r;
+}
+inline void readS(char *s)
+{
+	register int index = 0;
+	register char c = _getchar();
+
+	while (c < '!' || c > 'z')
+	{
+		c = _getchar();
+	}
+	while (c >= '!' && c <= 'z')
+	{
+		s[index++] = c;
+		c = _getchar();
+	}
+	s[index] = '\0';
+}
+inline void write(long long int l)
+{
+	if (!l) _putchar('0');
+	else
+	{
+		if (l < 0)
+		{
+			_putchar('-');
+			l = -l;
+		}
+		int index = 0, digits[21];
+		while (l)
+		{
+			digits[++index] = l % 10 + '0';
+			l /= 10;
+		}
+		for (int i = index + 1; --i;)
+		{
+			_putchar(digits[i]);
+		}
+	}
+}
+inline void writeS(const char *s)
+{
+	const char *end = s;
+	while (*end != '\0')
+	{
+		_putchar(*end);
+		++end;
+	}
+}
 
 
 int main()
