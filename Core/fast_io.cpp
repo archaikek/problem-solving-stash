21bd8bcabab3b826008819d7e51f66169c6526b9
Filip <archaikek>
2025-03-05 20:42:31 +0100

    Moved everything to main because SPOJ only accepts one file

--- Core/fast_io.cpp
+++ Core/fast_io.cpp
@@ -3,95 +3,95 @@
 void readI(int *i)
 {
 	register int r = 0;
-	register char c = getchar();
+	register char c = _getchar();
 	int z = (c == '-' ? -1 : 1);
 
 	while (c < '0' || c > '9')
 	{
-		c = getchar();
+		c = _getchar();
 		if (c == '-') z = -1;
 	}
 	while (c >= '0' && c <= '9')
 	{
 		r = 10 * r + c - '0';
-		c = getchar();
+		c = _getchar();
 	}
 	*i = r * z;
 }
 void readUI(int *i)
 {
 	register int r = 0;
-	register char c = getchar();
+	register char c = _getchar();
 
 	while (c < '0' || c > '9')
 	{
-		c = getchar();
+		c = _getchar();
 	}
 	while (c >= '0' && c <= '9')
 	{
 		r = 10 * r + c - '0';
-		c = getchar();
+		c = _getchar();
 	}
 	*i = r;
 }
 void readLL(long long int *l)
 {
 	register long long int r = 0;
-	register char c = getchar();
+	register char c = _getchar();
 	int z = (c == '-' ? -1 : 1);
 
 	while (c < '0' || c > '9')
 	{
-		c = getchar();
+		c = _getchar();
 		if (c == '-') z = -1;
 	}
 	while (c >= '0' && c <= '9')
 	{
 		r = 10 * r + c - '0';
-		c = getchar();
+		c = _getchar();
 	}
 	*l = r * z;
 }
 void readULL(long long int *l)
 {
 	register long long int r = 0;
-	register char c = getchar();
+	register char c = _getchar();
 
 	while (c < '0' || c > '9')
 	{
-		c = getchar();
+		c = _getchar();
 	}
 	while (c >= '0' && c <= '9')
 	{
 		r = 10 * r + c - '0';
-		c = getchar();
+		c = _getchar();
 	}
 	*l = r;
 }
 void readS(char *s)
 {
 	register int index = 0;
-	register char c = getchar();
+	register char c = _getchar();
 	
 	while (c < '!' || c > 'z')
 	{
-		c = getchar();
+		c = _getchar();
 	}
 	while (c >= '!' && c <= 'z')
 	{
 		s[index++] = c;
-		c = getchar();
+		c = _getchar();
 	}
 	s[index] = '\0';
 }
 void write(long long int l)
 {
-	if (!l) putchar('0');
+	if (!l) _putchar('0');
 	else
 	{
 		if (l < 0)
 		{
-			putchar('-');
+			_putchar('-');
 			l = -l;
 		}
 		int index = 0, digits[21];
@@ -102,7 +102,7 @@ void write(long long int l)
 		}
 		for (int i = index + 1; --i;)
 		{
-			putchar(digits[i]);
+			_putchar(digits[i]);
 		}
 	}
 }
@@ -111,7 +111,7 @@ void writeS(const char *s)
 	const char *end = s;
 	while (*end != '\0')
 	{
-		putchar(*end);
+		_putchar(*end);
 		++end;
 	}
 }
--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Repository for codes for competitive programming problem solving and similar.
 
 Different branches contain solutions for different problems, which will be linked in their respective readmes. 
 
-## master
-This branch contains common functions and macros for competitive programming.
+## spoj-ms2025-tdbfs
+Solution of the problem 'Searching the Graph' from https://www.spoj.com/MS2025/problems/TDBFS/. The contest will most likely be unavailable to outsiders.
+### Solution
