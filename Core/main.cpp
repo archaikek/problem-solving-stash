1d80696a8c7ff92cb49269479c8184a6a31304ad
Filip <archaikek>
2025-04-11 10:24:53 +0200

    Rough draft of the solution (still doesn't pass the tests)

--- Core/main.cpp
+++ Core/main.cpp
@@ -3,6 +3,7 @@
 #define ss second
 #define eb emplace_back
 #define debug if(0)
+#define helper
 
 typedef long long int LL;
 typedef long double LD;
@@ -142,6 +143,62 @@ void writeS(const char *s)
 	}
 }
 
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+
+#define INF 1000000007
+
+int n, m;
+int src, dst;
+int *rep, *union_size;
+bool *visited;
+int curr_order, *post_order;
+vector<int> *graph, *transposed;
+
+int find(int x)
+{
+	if (x == rep[x]) return x;
+	return rep[x] = find(rep[x]);
+}
+void onion(int x, int y)
+{
+	x = find(x);
+	y = find(y);
+
+	if (x == y) return;
+	if (union_size[x] < union_size[y]) swap(x, y);
+
+	rep[y] = rep[x];
+	union_size[x] += union_size[y];
+}
+
+void dfs1(const int x)
+{
+	visited[x] = true;
+	const int size = graph[x].size();
+	for (int i = 0; i < size; ++i)
+	{
+		if (!visited[graph[x][i]]) dfs1(graph[x][i]);
+	}
+	post_order[curr_order++] = x;
+}
+void dfs2(const int x)
+{
+	visited[x] = false;
+	const int size = transposed[x].size();
+	for (int i = 0; i < size; ++i)
+	{
+		helper int neighbour = transposed[x][i];
+		if (visited[neighbour])
+		{
+			onion(x, neighbour);
+			dfs2(neighbour);
+		}
+	}
+}
+
 int main()
 {
 #ifdef UNSAFE_IO
@@ -149,6 +206,58 @@ int main()
 	std::cout.tie(0);
 	std::ios_base::sync_with_stdio(0);
 #endif
+	readUI(&n);
+	rep =			(int *)malloc(n * sizeof(int));
+	union_size =	(int *)malloc(n * sizeof(int));
+	visited =		(bool *)calloc(n, sizeof(bool));
+	post_order =	(int *)malloc(n * sizeof(int));
+	graph =			new vector<int>[n];
+	transposed =	new vector<int>[n];
+	for (int i = 0; i < n; ++i)
+	{
+		rep[i] = i;
+		union_size[i] = 1;
+	}
+	readUI(&m);
 
+	for (int i = 0; i < m; ++i)
+	{
+		readUI(&src);
+		readUI(&dst);
+		graph[src].eb(dst);
+		transposed[dst].eb(src);
+	}
+
+	for (int i = 0; i < n; ++i) // calculate post orders for the graph
+	{
+		if (!visited[i]) dfs1(i);
+	}
+	for (int i = n - 1; i >= 0; --i)
+	{
+		if (visited[post_order[i]]) dfs2(post_order[i]); // traverse the transposed graph in reverse post order
+	}
+	for (int i = 0; i < n; ++i)
+	{
+		if (i < find(i))
+		{
+			rep[rep[i]] = rep[i] = i; // transfer the union representant to the lowest member of the union
+		}
+		write(find(i));
+		_putchar('\n');
+	}
+
+	delete[] transposed;
+	delete[] graph;
+	free(post_order);
+	free(visited);
+	free(union_size);
+	free(rep);
 	return 0;
 }
+/*
+3 3
+0 1
+1 0
+1 2
+
+*/
