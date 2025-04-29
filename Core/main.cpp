859a0ee354928f637bb783cd5277f5cf36f83fab
Filip <archaikek>
2025-04-29 05:18:40 +0200

    Switched to Tarjan's algorithm, tests passed

--- Core/main.cpp
+++ Core/main.cpp
@@ -144,59 +144,55 @@ void writeS(const char *s)
 }
 
 #include <cstdlib>
+#include <cstring>
 #include <vector>
 
 using namespace std;
 
-#define INF 1000000007
-
-int n, m;
+int n, m, node_index = 0;
 int src, dst;
-int *rep, *union_size;
-bool *visited;
-int curr_order, *post_order;
-vector<int> *graph, *transposed;
+vector<int> *graph;
 
-int find(int x)
+int *_index, *lowlink, *scc;
+bool *on_stack;
+vector<int> stack;
+
+void strongconnect(const int x)
 {
-	if (x == rep[x]) return x;
-	return rep[x] = find(rep[x]);
-}
-void onion(int x, int y)
-{
-	x = find(x);
-	y = find(y);
+	_index[x] = lowlink[x] = node_index++;
+	stack.eb(x);
+	on_stack[x] = true;
 
-	if (x == y) return;
-	if (union_size[x] < union_size[y]) swap(x, y);
-
-	rep[y] = rep[x];
-	union_size[x] += union_size[y];
-}
-
-void dfs1(const int x)
-{
-	visited[x] = true;
-	const int size = graph[x].size();
+	helper const int size = graph[x].size();
 	for (int i = 0; i < size; ++i)
 	{
-		if (!visited[graph[x][i]]) dfs1(graph[x][i]);
-	}
-	post_order[curr_order++] = x;
-}
-void dfs2(const int x)
-{
-	visited[x] = false;
-	const int size = transposed[x].size();
-	for (int i = 0; i < size; ++i)
-	{
-		helper int neighbour = transposed[x][i];
-		if (visited[neighbour])
+		helper const int neighbour = graph[x][i];
+		if (_index[neighbour] < 0) // neighbour not visited yet
 		{
-			onion(x, neighbour);
-			dfs2(neighbour);
+			strongconnect(neighbour);
+			lowlink[x] = min(lowlink[x], lowlink[neighbour]);
+		}
+		else if (on_stack[neighbour]) // neighbour on stack must belong to the same SCC
+		{
+			lowlink[x] = min(lowlink[x], _index[neighbour]);
+		} // otherwise x -> neighbour would be an edge pointing to another, already discovered SCC, and should be ignored
+	}
+	if (lowlink[x] == _index[x]) // no earlier node in the same SCC has been found, otherwise lowlink[x] would point to its index
+	{
+		for (;;)
+		{
+			helper const int y = stack.back();
+			stack.pop_back();
+			on_stack[y] = false;
+			scc[y] = x;
+			if (y == x) break;
 		}
 	}
+}
+int find(const int x)
+{
+	if (scc[x] == x) return x;
+	return scc[x] = find(scc[x]);
 }
 
 int main()
@@ -207,51 +203,41 @@ int main()
 	std::ios_base::sync_with_stdio(0);
 #endif
 	readUI(&n);
-	rep =			(int *)malloc(n * sizeof(int));
-	union_size =	(int *)malloc(n * sizeof(int));
-	visited =		(bool *)calloc(n, sizeof(bool));
-	post_order =	(int *)malloc(n * sizeof(int));
 	graph =			new vector<int>[n];
-	transposed =	new vector<int>[n];
-	for (int i = 0; i < n; ++i)
-	{
-		rep[i] = i;
-		union_size[i] = 1;
-	}
-	readUI(&m);
+	_index =		(int *)malloc(n * sizeof(int));
+	lowlink =		(int *)malloc(n * sizeof(int));
+	scc =			(int *)malloc(n * sizeof(int));
+	on_stack =		(bool *)calloc(n, sizeof(bool));
+	memset(_index, -1, n * sizeof(int));
+	memset(scc, -1, n * sizeof(int));
 
-	for (int i = 0; i < m; ++i)
+	readUI(&m);
+	while (m--)
 	{
 		readUI(&src);
 		readUI(&dst);
 		graph[src].eb(dst);
-		transposed[dst].eb(src);
 	}
 
-	for (int i = 0; i < n; ++i) // calculate post orders for the graph
+	for (int i = 0; i < n; ++i)
 	{
-		if (!visited[i]) dfs1(i);
-	}
-	for (int i = n - 1; i >= 0; --i)
-	{
-		if (visited[post_order[i]]) dfs2(post_order[i]); // traverse the transposed graph in reverse post order
+		if (_index[i] < 0) strongconnect(i);
 	}
 	for (int i = 0; i < n; ++i)
 	{
-		if (i < find(i))
+		if (i < find(i)) // reconnect SCC ownership to the lowest-indexed node
 		{
-			rep[rep[i]] = rep[i] = i; // transfer the union representant to the lowest member of the union
+			scc[find(i)] = i;
+			scc[i] = i;
 		}
-		write(find(i));
-		_putchar('\n');
+		printf("%d\n", find(i));
 	}
 
-	delete[] transposed;
+	free(on_stack);
+	free(scc);
+	free(lowlink);
+	free(_index);
 	delete[] graph;
-	free(post_order);
-	free(visited);
-	free(union_size);
-	free(rep);
 	return 0;
 }
 /*
--- README.md
+++ README.md
@@ -3,7 +3,8 @@ Repository for codes for competitive programming problem solving and similar.
 
 Different branches contain solutions for different problems, which will be linked in their respective readmes. 
 
-## master
-This branch contains common functions and macros for competitive programming.
+## spoj-ms2025-webisl
+Branch for the solution of the Web islands problem at https://www.spoj.com/MS2025/problems/WEBISL/ (which is probably unavailable to outsiders)
 
-Many contests require (or at least highly encourage) writing the entire code in a single file, so this makes for a nice base for those (so that I don't have to make that move myself for every exercise).
+### Solution
+Use Tarjan's algorithm to find strongly connected components in the graph, and then a find-and-union-like approach to reindex them to the lowest index of a node in a given SCC
